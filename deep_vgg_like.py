05a6ae0685a2596b35ee02041f52253702633377
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-08-02 18:46:10 +0200

    test for 110 vgg-like

--- deep_vgg_like.py
+++ deep_vgg_like.py
@@ -158,7 +158,7 @@ def train_network(model, batch_size=128):
 
     history = model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
                                   validation_data=(x_test, y_test),
-                                  epochs=300,
+                                  epochs=1,  # 300,
                                   verbose=2,
                                   workers=4,
                                   steps_per_epoch=x_train.shape[0] /
@@ -168,7 +168,7 @@ def train_network(model, batch_size=128):
 
 
 if __name__ == '__main__':
-    depth = 56
+    depth = 110
     input_shape = x_train.shape[1:]
 
     model = vgg_like(input_shape=input_shape, depth=depth)
